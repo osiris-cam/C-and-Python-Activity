e16671850e216692fe5f1106734159d7fe283e6f
osiris-cam <59631231+osiris-cam@users.noreply.github.com>
2020-05-28 22:43:52 -0500

    C

---
+++ C/Problem2.c
@@ -0,0 +1,16 @@
+//Write a program to check whether a number is even or odd.
+//Expected Output: Even or Odd
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    int x;  //Declare the variable
+    printf("Write a number, please: \n");
+    scanf("%d", &x);    //Scan and asign a value to the variable
+    if (x % 2 == 0){    //If the number can be divided by 2 and the residue is O print even
+        printf("Even");
+    } else{ //Else print odd
+        printf("Odd");
+    }
+    return 0;
+}
